7ec033e7339caebee575225cfcad79339a6f62d0
SenWangHost <senwang1992010@gmail.com>
2018-10-08 14:29:53 -0400

    set up the test client and test the connectivity between client and server

---
+++ test.js
@@ -0,0 +1,21 @@
+/**
+ * the client side of zeromq is used to conduct test
+ */
+const zmq = require("zeromq");
+
+// socket to talk to server
+console.log("Starting the test clinet…");
+const requester = zmq.socket('req');
+
+requester.on("message", (reply) => {
+    console.log("Received reply: [" + reply.toString() + "]");
+});
+
+requester.connect("tcp://localhost:1984");
+
+// for basic test
+requester.send("Hello");
+
+process.on("SIGINT", () => {
+    requester.close();
+});
