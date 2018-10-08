9a410a3edffa98f52e3d613d002ce978566a8e88
SenWangHost <senwang1992010@gmail.com>
2018-10-08 15:10:24 -0400

    set up the transaction service module for interacting with mongodb

---
+++ services/transactionService.js
@@ -0,0 +1,29 @@
+const transactionModel = require("../models/transactionModel");
+
+/**
+ * insert array of new transactions into the transaction collection
+ * update the recurring group for recurring group collection 
+ */
+const insertTransaction = (newTransactions) => {
+
+};
+
+/**
+ * get the recurring group of one user specified by user_id
+ */
+const getRecurringGroupsByUser = (user_id) => {
+
+};
+
+/**
+ * get all recurring groups
+ */
+const getAllRecurringGroups = () => {
+
+};
+
+module.exports = {
+    insertTransaction: insertTransaction,
+    getRecurringGroupsByUser: getRecurringGroupsByUser,
+    getAllRecurringGroups: getAllRecurringGroups
+};
